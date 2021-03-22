c2148adcfcdb4a03d19a064ae000030d30496f97
KennethSC <kennethsantacruz12@gmail.com>
2021-03-22 00:42:10 -0700

    First Commit

---
+++ app/todo/task/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+class TaskView(View):
+    def get(self, request):
+        return render(request, 'task/tasks.html', context = {'name': 'RED'})
+
+# Create your views here.
